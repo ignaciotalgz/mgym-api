cfd847534e8359ac0522bce56442a46332b9a7a8
ignaciotalgz <ignaciotalgz@gmail.com>
2026-03-21 15:07:10 -0300

    Commit inicial

---
+++ src/main/java/com/mgym/mgym/entidades/Usuario.java
@@ -0,0 +1,5 @@
+package com.mgym.mgym.entidades;
+
+public class Usuario {
+
+}
---
+++ src/test/java/com/mgym/mgym/MgymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgym.mgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
